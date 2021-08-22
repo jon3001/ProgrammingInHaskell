c579ab5b9eda4cfad0812e1662c3cd8be37af83e
Jonathan Lonsadale <jon@jlonsdale.net>
2021-08-22 22:43:09 +0100

    Refine comments in exercise

--- 02/mytest.hs
+++ 02/mytest.hs
@@ -1,6 +1,6 @@
 double x = x + x
 quadruple x = double(double x)
 factorial n = product[1..n]
- 
+
 -- In general, any function with two arguments can be written between its arguments by enclosing the name of the function in single back quotes
-average ns = sum ns `div` length ns  -- average ns = div (sum ns) (length ns) -- was also possible
+average ns = sum ns `div` length ns  -- average ns = div (sum ns) (length ns) -- was also possible; x `f` y ... syntactic sugar for f x y
